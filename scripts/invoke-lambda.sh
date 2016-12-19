83aa3b5a183c888058886cb32e48072a33805ac3
Bruno Kenji <bruno.furukawa@dev.codus.com.br>
2016-12-19 14:05:05 -0200

    added invoke-lambda script

--- scripts/invoke-lambda.sh
+++ scripts/invoke-lambda.sh
@@ -2,6 +2,11 @@
 
 #!/bin/bash
 
+# .main.sh provides API_ID
+. ./support/aws-scripts/.main.sh
+
+API_NAME="$(aws apigateway get-rest-api --rest-api-id $API_ID | jq -r '.name')"
+
 # can receive lambda name
 if [ $1 ]
 then
@@ -10,7 +15,7 @@ else
   read -p "Lambda name (not the file name!): " LAMBDA_NAME
 fi
 
-read -p "Arguments (blob): " INVOKE_ARGS
+INVOKE_ARGS="${API_NAME}/${LAMBDA_NAME}/invocation-params.json"
 
 invokeLambda() {
   aws lambda invoke-async \
